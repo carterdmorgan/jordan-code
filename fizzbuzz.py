73acb94ce8d93b1efead7de42e8f80d03fa0af71
Jordan Morgan <jordan.morgan999@gmail.com>
2024-09-10 12:22:51 -0600

    Updated fizzbuzz.py to remove fizzbuzz class and clean up if block in fizzbuzz function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,53 +1,40 @@
-class Fizzbuzz:
-    def __init__(self, fb_list = None) -> None:
-        if fb_list is None:
-            fb_list = []
-        self.fb_list = fb_list
 
-    def fizzbuzz(self, num = 0):
-        if num < 0 or num > pow(10, 4):
-            raise ValueError('invalid number')
-        for i in range(1, num+1):
-            if i % 3 == 0 and i % 5 == 0:
-                self.fb_list.append('FizzBuzz')
-            elif i % 3 == 0 and i % 5 != 0:
-                self.fb_list.append('Fizz')
-            elif i % 3 != 0 and i % 5 == 0:
-                self.fb_list.append('Buzz')
-            elif i % 3 != 0 and i % 5 != 0:
-                self.fb_list.append(str(i))
-        return self.fb_list
+
+
+def fizzbuzz(num = 0):
+    fizz_list = []
+
+    if num < 0 or num > pow(10, 4):
+        raise ValueError('invalid number')
+    
+    for i in range(1, num+1):
+        if i % 3 == 0 and i % 5 == 0:
+            fizz_list.append('FizzBuzz')
+        elif i % 3 == 0:
+            fizz_list.append('Fizz')
+        elif i % 5 == 0:
+            fizz_list.append('Buzz')
+        elif i % 3 != 0 and i % 5 != 0:
+            fizz_list.append(str(i))
+    return fizz_list
+
+# quick function to test fizzbuzz
+def test_fizzbuzz(num = 0):
+    try:
+        print(fizzbuzz(num))
+    except ValueError as e:
+        print({e})    
     
 
 def main():
+    # expect [1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz]
+    test_fizzbuzz(15)
 
-    # simple test of fizzbuzz.fizzbuzz method
-    def fizzbuzz_test(item: Fizzbuzz, num = 0):
-        try:
-            item.fizzbuzz(num)
-            print(item.fb_list)
-        except ValueError as e:
-            print({e})
+    # expect caught ValueError
+    test_fizzbuzz(-1)
 
-    item1 = Fizzbuzz()
-    item2 = Fizzbuzz()
+    # expect caught ValueError
+    test_fizzbuzz(pow(10, 16))
 
-    # expect no errors
-    fizzbuzz_test(item1, 5)
-
-    # expect ValueError caught
-    fizzbuzz_test(item1, -1)
-
-    # expect ValueError caught
-    fizzbuzz_test(item1, pow(10, 5))
-
-        # expect no errors
-    fizzbuzz_test(item2, 9)
-
-    # expect ValueError caught
-    fizzbuzz_test(item2, -1)
-
-    # expect ValueError caught
-    fizzbuzz_test(item2, pow(10, 5))
 
 main()
