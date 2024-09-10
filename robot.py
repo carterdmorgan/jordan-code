02727038e32144dfbfc47ced3dc2d9264bb3fd7e
Carter Morgan <carterdmorgan@gmail.com>
2024-09-09 20:14:57 -0600

    pushing jordan's new code

--- robot.py
+++ robot.py
@@ -1,3 +1,5 @@
+
+
 class Robot:
     COMPASS = {
         0:   'north',
@@ -9,59 +11,61 @@ class Robot:
         270: 'west',
         315: 'northwest'
     }
-
-    ANGLE_STEP = 45
+    degree_turn = 45
 
     def __init__(self, point = 0) -> None:
         self.point = point
 
     # ensures result of direction calculation below is between 0 and 360
     def circle(self):
-        self.point % 360
-    
-    def turn_right(self) -> None:
-        self.point += Robot.ANGLE_STEP
-        self.circle()
-    
-    def turn_left(self) -> None:
-        self.point -= Robot.ANGLE_STEP
-        self.circle()
+        self.point %= 360
 
-    # rounding to nearest mutliple of 45 to correspond to 8 directions, remove if more precision is developed. 
-    def round(self, round_point):
-        return Robot.ANGLE_STEP*(round(round_point/Robot.ANGLE_STEP,0))
-    
-    def get_current_direction(self):
-        return Robot.COMPASS[self.round(self.point)]
+    # turn robot left
+    def turn_left(self):
+        self.point -= Robot.degree_turn
+        self.circle()
+        print(f'Now facing {Robot.COMPASS[self.point]}\n')
+
+    # turn robot right
+    def turn_right(self):
+        self.point += Robot.degree_turn
+        self.circle()
+        print(f'Now facing {Robot.COMPASS[self.point]}\n')
 
 
 def main():
-    robot_map = {}
-    current_robot = Robot()
-    current_key = '0'
-    robot_map[current_key] = current_robot
+    robot_dict = {}
+    print('Activating robot...\n')
+    # start with a robot
+    robot_dict['0'] = Robot()
+    current_robot = robot_dict['0']
 
-    # allowed commands
-    valid_commands = ['L', 'R', 'Q']
+    # allowed commands from user
+    command_list = ['L', 'R', 'E']
 
-    while True:
-        current_input = input('What would you like to do?\n')
-        if current_input.isdigit():
-            # TODO: Initialize new robot
-            print("valid digit command")
-        elif current_input in valid_commands:
-            if current_input == "L":
-                current_robot.turn_left()
-            elif current_input == "R":
-                current_robot.turn_right()
-            elif current_input == "Q":
-                break
+    while True: 
+        user_action = input('What would you like to do?\n').upper()
+        # digit entered either activates that robot or creates and activates a new one
+        if user_action.isdigit():
+            if user_action in robot_dict.keys():
+                print(f'Now controlling robot {user_action}\n')
+            elif user_action not in robot_dict.keys():
+                robot_dict[user_action] = Robot()
+                print(f'Robot {user_action} created. Now controlling robot {user_action}\n')
 
-            print(f"Robot {current_key} is facing {current_robot.get_current_direction()}")
-        else:
-            print("Invalid command. Please try again.")
-    
-    print("Robot shutting down.")
+            current_robot = robot_dict[user_action]
+            
+        elif user_action not in command_list:
+            print('Invalid command\n')
 
+        elif user_action == 'L':
+            current_robot.turn_left()
+
+        elif user_action == 'R':
+            current_robot.turn_right()
+
+        elif user_action == 'E':
+            print('Exiting...')
+            break
 
 main()
