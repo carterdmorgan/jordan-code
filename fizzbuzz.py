8b2e224dabb4b72634c69f156b781cbaa836f41f
Jordan Morgan <jordan.morgan999@gmail.com>
2024-09-10 23:01:51 -0600

    removed self-built test from fizzbuss

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,3 @@
-
-
-
 def fizzbuzz(num = 0):
     fizz_list = []
 
@@ -18,23 +15,6 @@ def fizzbuzz(num = 0):
             fizz_list.append(str(i))
     return fizz_list
 
-# quick function to test fizzbuzz
-def test_fizzbuzz(num = 0):
-    try:
-        print(fizzbuzz(num))
-    except ValueError as e:
-        print({e})    
-    
-
 def main():
-    # expect [1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz]
-    test_fizzbuzz(15)
-
-    # expect caught ValueError
-    test_fizzbuzz(-1)
-
-    # expect caught ValueError
-    test_fizzbuzz(pow(10, 16))
-
-
+    print(fizzbuzz(15))
 main()
