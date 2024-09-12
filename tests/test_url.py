28c89822384deca400fbb8c0e18d0fcec0584647
Jordan Morgan <jordan.morgan999@gmail.com>
2024-09-11 19:15:11 -0600

    built url.py

---
+++ tests/test_url.py
@@ -0,0 +1,45 @@
+import unittest, sys, os
+
+sys.path.append(os.path.abspath('..'))
+
+from url import URL
+
+class URLTest(unittest.TestCase):
+
+    def setUp(self):
+        self.long_url = 'www.homestarrunner.com'
+        self.short_url = '4e0080'
+
+    def test_shorten_url(self):
+        test_short_url = URL.shorten_url(self.long_url)
+        self.assertEqual(self.short_url, test_short_url)
+
+    def test_shorten_url_malformed(self):
+        with self.assertRaises(ValueError):
+            URL.shorten_url('ww.homestarrunner.com')
+
+    def test_get_long_url(self):
+        test_short_url = URL.shorten_url(self.long_url)
+        test_long_url = URL.get_long_url(test_short_url)
+        self.assertEqual(self.long_url, test_long_url)
+
+    def test_get_long_url_missing(self):
+        with self.assertRaises(ValueError):
+            URL.get_long_url(self.short_url)
+    
+    def test_get_access_count(self):
+        URL.shorten_url(self.long_url)
+        URL.get_long_url(self.short_url)
+        test_access_count = URL.get_access_count(self.short_url)
+        self.assertEqual(1, test_access_count)
+
+    def test_get_access_count_missing(self):
+        with self.assertRaises(ValueError):
+            URL.get_access_count(self.short_url)
+
+
+
+    
+
+
+
